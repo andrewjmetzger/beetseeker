088811c5d2d79fc5f7aa0fc7d5e8269dd2312942
Andrew Metzger <andrewjmetzger@users.noreply.github.com>
2023-12-03 15:44:51 -0500

    config shouldn't shadow the secrets stdlib

---
+++ example_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# You must customize this file and rename it to config.py
+
+# Path to the 'completed downloads' directory for slskd
+DOWNLOADS_DIRECTORY = "/path/to/slskd/completed/downloads"
+
+# URL and API key for slskd
+SLSKD_URL = "http://localhost:5000"
+SLSKD_API_KEY = "your_slskd_api_key"
+
+# URL and API key for betanin
+BETANIN_URL = "http://localhost:8000"
+BETANIN_API_KEY = "your_betanin_api_key"
